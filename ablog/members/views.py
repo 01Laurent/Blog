3a666f90c6c2d98071ae8f657169446fea9a9b53
01Laurent <kiirulaurent@gmail.com>
2024-08-27 15:22:45 +0300

    edit profile

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,6 +1,8 @@
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm
 
@@ -9,3 +11,10 @@ class UserRegisterView(generic.CreateView):
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
