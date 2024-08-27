561b797ff424c0a70097e34e8a53a6d7a23cb160
01Laurent <kiirulaurent@gmail.com>
2024-08-27 19:45:01 +0300

    images

--- ablog/ablog/urls.py
+++ ablog/ablog/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('members/', include('members.urls')),
     # ckeditor 5
     path('ckeditor5/', include('django_ckeditor_5.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
