c05176e3da89a6e75d2fa5252a2017e8cb99ad29
01Laurent <kiirulaurent@gmail.com>
2024-08-29 11:44:42 +0300

    account creation

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,13 +1,27 @@
 from django.db.models.base import Model as Model
 from django.db.models.query import QuerySet
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm, PasswordsChangingForm
+from .forms import SignUpForm, EditProfileForm, PasswordsChangingForm, ProfilePageForm
 from myblog.models import Profile
+from django.contrib import messages
+
+class CreateProfilePageView(CreateView):
+    model = Profile
+    form_class = ProfilePageForm
+    template_name = 'registration/create_user_profile_page.html'
+    # fields = '__all__'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "Created successfuly.")
+        return super().form_valid(form)
 
 class EditProfilePageView(generic.UpdateView):
     model = Profile
@@ -37,6 +51,13 @@ class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Registration successful.")
+        return super().form_valid(form)
+    def form_invalid(self, form):
+        print(form.errors)
+        return super().form_invalid(form)
 
 class UserEditView(generic.UpdateView):
     form_class = EditProfileForm
